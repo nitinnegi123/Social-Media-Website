aee47cfebe276dfb9105af8c5f78a8d6dcfdbc99
NITIN <nikinegi007@gmail.com>
2022-07-31 18:20:50 +0530

    add home and contoller for home

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const app=express();
 const port=8000;
+//use express router
+app.use('/',require('./routes/index.js'))
 app.listen(port,function(error)
 {
     if(error)
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const router=express.Router()
+console.log('STARTED');
+const homeController=require('../controllers/home_controller')
+router.get('/',homeController.home)
+
+
+
+
+
+
+
+module.exports=router;
