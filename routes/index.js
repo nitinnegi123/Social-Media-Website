885224fb8bc87a0d6037617aa8da5c826a11d070
NITIN <nikinegi007@gmail.com>
2022-07-31 19:18:42 +0530

    routes and controller done

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express=require('express');
 const router=express.Router()
 console.log('STARTED');
 const homeController=require('../controllers/home_controller')
-router.get('/',homeController.home)
-
+router.get('/',homeController.home);
+router.use('/users',require('./users'))
+//for any another routes access from here
+//router.use('./routerName',require('./routerfield'))
 
 
 
