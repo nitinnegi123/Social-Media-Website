66c4e91ae99f3ee27670d802c9997ec727e5e6d7
NITIN <nikinegi007@gmail.com>
2022-08-02 20:18:01 +0530

    parital and layouts done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const app=express();
+const expressLayouts=require('express-ejs-layouts');
+app.use(expressLayouts);//tell befoore layout is using
 const port=8000;
 //use express router
 app.use('/',require('./routes/index.js'))
