5ef54315b4a3e6fcaf92e2b465def824324c0b02
NITIN <nikinegi007@gmail.com>
2022-08-04 21:15:35 +0530

    setting up view engine and static files

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app=express();
 const expressLayouts=require('express-ejs-layouts');
 app.use(expressLayouts);//tell befoore layout is using
 const port=8000;
+//to use before view
+app.use(express.static('./assests'));
+//extract style and scripts from sub pages to layout
+app.set("layout extractStyles",true)
+app.set("layout extractScripts",true)
 //use express router
 app.use('/',require('./routes/index.js'))
 //set up view engine
