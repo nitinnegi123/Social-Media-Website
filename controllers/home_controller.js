6ff2348f9e79bc20f4aade808252ab9e56d25bd4
NITIN <nikinegi007@gmail.com>
2022-08-18 21:25:19 +0530

    post show done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,25 @@
+const Post=require('../models/post')
+const { post } = require('../routes/posts')
 module.exports.home=(req,res)=>
 {
     // res.cookie('user_id',25);//chnaging at response
     //console.log(req.cookies) //coming from request
-    return res.render('home',
-    {
-        title:"home"
-    })
+//    Post.find({},function(err,posts)
+//    {
+//         return res.render('home',
+//         {
+//             title:"home",
+//             posts:posts
+//         })
+//    })
+//when query get longer use exec for callback
+   Post.find({}).populate('user').exec(function(err,posts)
+   {
+        return res.render('home',
+        {
+            title:"home",
+            posts:posts
+        })
+   })
 }
 // module.exports.actionName = function(req, res){}
