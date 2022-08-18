7df65f82ce63739e2fcc92b117069bafd53c6964
NITIN <nikinegi007@gmail.com>
2022-08-18 21:38:13 +0530

    post done

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const passport=require('passport')
 
 const postsController = require('../controllers/posts_controller');
-
-router.post('/create', postsController.create);
+//double check
+router.post('/create',passport.checkAuthentication ,postsController.create);
 
 
 module.exports = router;
